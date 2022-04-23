bd303e0178ab565e47de3dfdc8daf74f78efdc5d
Kadir Cem Yılmaz <kadircemyilmaz@ogrenci.beykoz.edu.tr>
2022-04-23 16:05:59 +0300

    Nothing

--- oygemHackathon/Assets/Scripts/CarManager.cs
+++ oygemHackathon/Assets/Scripts/CarManager.cs
@@ -7,13 +7,16 @@ public class CarManager : MonoBehaviour
 {
     public tilesetController tileScript;
     public float speed;
-    int i = 0;
-    bool start = false;
-    List<Transform> points;
-
     
+    bool start = false;
+    public List<GameObject> points;
+    
+
+
+
     void Start()
     {
+        
         tileScript = GameObject.Find("Main Camera").GetComponent<tilesetController>();
         //Vector2.Lerp(gameObject.transform.position, tileScript.points[0].transform.position, speed);
     }
@@ -21,6 +24,8 @@ public class CarManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        
         for (int i = 0; i < tileScript.points.Count; i++)
         {
             if (Vector2.Distance(gameObject.transform.position, tileScript.points[i].transform.position) <= 0.2f)
@@ -28,6 +33,7 @@ public class CarManager : MonoBehaviour
                 tileScript.points.Remove(tileScript.points[i]);
 
             }
+
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
@@ -35,38 +41,37 @@ public class CarManager : MonoBehaviour
         }
         if (start)
         {
-            
-            
-            transform.position = Vector2.MoveTowards(gameObject.transform.position, GetClosestPoint(tileScript.points).position, speed * Time.deltaTime);
 
-            
-            
+            transform.position = Vector2.MoveTowards(gameObject.transform.position, GetClosestPoint(tileScript.points).position, speed * Time.deltaTime);
+            start = false;
         }
-        
+
     }
     Transform GetClosestPoint(List<GameObject> enemies)
     {
+        
+
         Transform tMin = null;
         float minDist = Mathf.Infinity;
         Vector3 currentPos = transform.position;
         foreach (GameObject t in enemies)
         {
-            float dist = Vector3.Distance(t.transform.position, currentPos);
-            if (dist < minDist && minDist > 1)
-            {
-                tMin = t.transform;
-                minDist = dist;
-               
-            }
-            
+
+                float dist = Vector3.Distance(t.transform.position, currentPos);
+                Debug.Log(t.transform.position.x);
+                if (dist < minDist)
+                {
+
+                //potentialTiles.Add(t);
+
+                    tMin = t.transform;
+                    minDist = dist;
+                }
+
         }
-        
-        Debug.Log(tMin);
+       
         return tMin;
     }
-    private void FixedUpdate()
-    {
-        
+   
 
-    }
 }
--- oygemHackathon/Assets/Scripts/tilesetController.cs
+++ oygemHackathon/Assets/Scripts/tilesetController.cs
@@ -5,6 +5,8 @@ using UnityEngine.Tilemaps;
 
 public class tilesetController : MonoBehaviour
 {
+    public CarManager carScript;
+
     public Tilemap tiles;
     public Tile tile;
     public Vector3Int location;
@@ -12,6 +14,8 @@ public class tilesetController : MonoBehaviour
     public GameObject movePoint;
 
     public List<GameObject> points;
+
+    public GameObject[] whichPoint;
     void Start()
     {
         
@@ -22,20 +26,24 @@ public class tilesetController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-           
+            
+
             //
             //Debug.Log(Camera.main.ScreenPointToRay(Input.mousePosition));
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             location = tiles.WorldToCell(pos);
+            
 
             tiles.SetTile(location , tile);
             
             
-            GameObject point = Instantiate(movePoint);
+            GameObject point = Instantiate(whichPoint[0]);
+
             point.transform.position = new Vector3(tiles.CellToWorld(location).x, tiles.CellToWorld(location).y+0.5f, tiles.CellToWorld(location).z);
 
             points.Add(point);
             
+
         }
         else if (Input.GetMouseButtonDown(1))
         {
@@ -44,5 +52,11 @@ public class tilesetController : MonoBehaviour
             Debug.Log(tiles.GetTile(location).name);
         }
     }
-    
+    private void OnMouseDown()
+    {
+        
+    }
+
+  
+
 }

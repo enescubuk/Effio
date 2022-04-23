fa658542fff0a18f531b97c318db4f3edb59abbc
Enes <enescubuk10@gmail.com>
2022-04-23 15:42:31 +0300

    ui rezerve

--- oygemHackathon/Assets/Scripts/positionControl.cs
+++ oygemHackathon/Assets/Scripts/positionControl.cs
@@ -7,8 +7,8 @@ public class positionControl : MonoBehaviour
 {
     public Text coinText;
     private int coin=120;
-    public Ray selectedTiled;
-    
+    public GameObject panel;
+    //public Camera camera;
     void Start()
     {
         
@@ -18,68 +18,63 @@ public class positionControl : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log(Camera.main.ScreenPointToRay(Input.mousePosition));
-            selectedTiled = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Debug.Log(selectedTiled);
+            panel.SetActive(true);
+            panel.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            panel.transform.position = new Vector3(panel.transform.position.x,panel.transform.position.y,0);
+            Debug.Log(Input.mousePosition);
             
         }
-
         coinText.text = "" + coin;
     }
 
     public void path1()
     {
         coin -= 50;
-
+        panel.SetActive(false);
     }
     public void path2()
     {
-        
         coin -= 50;
-        
+        panel.SetActive(false);
     }
     public void path3()
     {
         coin -= 50;
-        
+        panel.SetActive(false);
     }
     public void path4()
     {
         coin -= 90;
-        
+        panel.SetActive(false);
     }
     public void path5()
     {
         coin -= 90;
-        
+        panel.SetActive(false);
     }
     public void path6()
     {
         coin -= 90;
-        
+        panel.SetActive(false);
     }
     public void path7()
     {
-        
         coin -= 30;
-        
+        panel.SetActive(false);
     }
     public void path8()
     {
-        
         coin -= 30;
-        
+        panel.SetActive(false);
     }
     public void path9()
     {
-       
         coin -= 30;
-        
+        panel.SetActive(false);
     }
     public void path10()
     {
-       
         coin -= 120;
-
+        panel.SetActive(false);
     }
 }

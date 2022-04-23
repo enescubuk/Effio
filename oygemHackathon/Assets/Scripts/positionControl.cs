a6b6fdcbda1e08c269a2b14e391aac07a025de09
emirbaykal <83282141+emirbaykal@users.noreply.github.com>
2022-04-23 14:25:00 +0300

    scroll

--- oygemHackathon/Assets/Scripts/positionControl.cs
+++ oygemHackathon/Assets/Scripts/positionControl.cs
@@ -8,7 +8,7 @@ public class positionControl : MonoBehaviour
     public Text coinText;
     private int coin=120;
     public Ray selectedTiled;
-    public GameObject selectedTiledCanvas;
+    
     void Start()
     {
         
@@ -21,7 +21,7 @@ public class positionControl : MonoBehaviour
             Debug.Log(Camera.main.ScreenPointToRay(Input.mousePosition));
             selectedTiled = Camera.main.ScreenPointToRay(Input.mousePosition);
             Debug.Log(selectedTiled);
-            selectedTiledCanvas.SetActive(false);
+            
         }
 
         coinText.text = "" + coin;
@@ -29,61 +29,56 @@ public class positionControl : MonoBehaviour
 
     public void path1()
     {
-        selectedTiledCanvas.gameObject.SetActive(false);
         coin -= 50;
 
     }
     public void path2()
     {
-        selectedTiledCanvas.gameObject.SetActive(false);
+        
         coin -= 50;
         
     }
     public void path3()
     {
-        selectedTiledCanvas.gameObject.SetActive(false);
         coin -= 50;
         
     }
     public void path4()
     {
-        selectedTiledCanvas.gameObject.SetActive(false);
         coin -= 90;
         
     }
     public void path5()
     {
-        selectedTiledCanvas.gameObject.SetActive(false);
         coin -= 90;
         
     }
     public void path6()
     {
-        selectedTiledCanvas.gameObject.SetActive(false); 
         coin -= 90;
         
     }
     public void path7()
     {
-        selectedTiledCanvas.gameObject.SetActive(false);
+        
         coin -= 30;
         
     }
     public void path8()
     {
-        selectedTiledCanvas.gameObject.SetActive(false);
+        
         coin -= 30;
         
     }
     public void path9()
     {
-        selectedTiledCanvas.gameObject.SetActive(false);
+       
         coin -= 30;
         
     }
     public void path10()
     {
-        selectedTiledCanvas.gameObject.SetActive(false);
+       
         coin -= 120;
 
     }
